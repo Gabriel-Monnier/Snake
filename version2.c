7669665e7ad03f1f4a5a22d67f886cb4658a4518
Gabriel-Monnier <gabrielmonnier@orange.fr>
2024-11-06 09:53:25 +0100

    version 2 réparation d'un  bug

--- version2.c
+++ version2.c
@@ -22,7 +22,7 @@ colonne x, puis un numéro de ligne y. L’écran devra ensuite s’effacer et a
 #include <stdbool.h>
 
 // déclaration des constante
-#define TAILLE_S 10 // taille du serpent
+#define TAILLE_S 10   // taille du serpent
 #define TAILLE_MAX 40 // taille du tableau
 #define TAILLE_MIN 1
 
@@ -41,10 +41,9 @@ void afficher(int x, int y, char c);
 void effacer(int x, int y);
 void dessinerSerpent(int lesX[], int lesY[]);
 void progresser(int lesX[], int lesY[], char direction);
-int  kbhit();
+int kbhit();
 void disableEcho();
 void enableEcho();
-
 
 /**
  * @brief Entrée du programme
@@ -62,15 +61,14 @@ int main()
     // on demande à l'utilisateur la position de depart du serpent
     boucle = true;
     direction = DROITE;
-    do 
+    do
     {
         printf("Entrez la position initiale de la tete du serpent (colonne 1-40, ligne 1-40):\n");
         printf("Colonne (x): ");
         scanf("%d", &x);
         printf("Ligne (y): ");
         scanf("%d", &y);
-    }
-    while((x < TAILLE_MIN) || (x > TAILLE_MAX) || (y < TAILLE_MIN) || (y > TAILLE_MAX)); // on verifie que la position est bonne sinon on redemande
+    } while ((x < TAILLE_MIN) || (x > TAILLE_MAX) || (y < TAILLE_MIN) || (y > TAILLE_MAX)); // on verifie que la position est bonne sinon on redemande
     system("clear");
     // on initialise la position du serpent au coordonnées voulu
     for (int i = 0; i < TAILLE_S; i++)
@@ -91,28 +89,28 @@ int main()
             }
             else if (c == DROITE)
             {
-                if (direction != GAUCHE )
+                if (direction != GAUCHE)
                 {
                     direction = DROITE;
                 }
             }
             else if (c == GAUCHE)
             {
-                if (direction != DROITE )
+                if (direction != DROITE)
                 {
                     direction = GAUCHE;
                 }
             }
             else if (c == HAUT)
             {
-                if (direction != BAS )
+                if (direction != BAS)
                 {
                     direction = HAUT;
                 }
             }
             else if (c == BAS)
             {
-                if (direction != HAUT )
+                if (direction != HAUT)
                 {
                     direction = BAS;
                 }
@@ -121,13 +119,13 @@ int main()
         else
         {
             effacer(positionX[TAILLE_S - 1], positionY[TAILLE_S - 1]); // efface le bout du serpent qui ne seras pas remplacer par le prochain serpent
-            progresser(positionX, positionY, direction); // met a jour la position du serpent
-            dessinerSerpent(positionX, positionY); // dessiner le nouveau serpent a la position mis a jour
-            usleep(TEMPORISATION); // attend 500 milliseconde
+            progresser(positionX, positionY, direction);               // met a jour la position du serpent
+            dessinerSerpent(positionX, positionY);                     // dessiner le nouveau serpent a la position mis a jour
+            usleep(TEMPORISATION);                                     // attend 500 milliseconde
         }
     }
     enableEcho();
-    system("clear");// efface l'écran à la fin
+    system("clear"); // efface l'écran à la fin
     return EXIT_SUCCESS;
 }
 
@@ -141,7 +139,6 @@ void gotoXY(int x, int y)
     printf("\033[%d;%df", y, x);
 }
 
-
 /**
  * @brief Procédure pour afficher le caractère c à la position (x, y)
  * @param ens de type entier : la colonne sur laquelle on veut le curseur
@@ -150,8 +147,11 @@ void gotoXY(int x, int y)
  */
 void afficher(int x, int y, char c)
 {
-    gotoXY(x, y);
-    printf("%c", c);
+    if (x >= TAILLE_MIN && x <= TAILLE_MAX && y >= TAILLE_MIN && y <= TAILLE_MAX)
+    {
+        gotoXY(x, y);
+        printf("%c", c); // Si les coordonnées ne sont pas en dehors des limites, afficher le caractère
+    }
 }
 
 /**
@@ -166,8 +166,8 @@ void effacer(int x, int y)
 }
 
 /**
- * @brief Procédure pour afficher le serpent en utilisant les tableaux donnés 
- * en premier la tête à la première position suivie du corp suivie du corps 
+ * @brief Procédure pour afficher le serpent en utilisant les tableaux donnés
+ * en premier la tête à la première position suivie du corp suivie du corps
  * qui est dessiné à partir de la  deuxième position jusqu'à la fin du tableau.
  * @param ens lesX[] tableau d'entiers : les coordonnées de la colonne du serpent.
  * @param ens lesY[] tableau d'entiers : les coordonnées de la ligne du serpent.
@@ -181,10 +181,9 @@ void dessinerSerpent(int lesX[], int lesY[])
     }
 }
 
-
 /**
  * @brief Procédure pour mettre à jour la position du serpent
- * on déplace chaque bout du serpent vers la position de devant 
+ * on déplace chaque bout du serpent vers la position de devant
  * la tête du serpent est mise à la droite du corp.
  * @param lesX[] tableau d'entiers : les coordonnées actuelle des colonnes du serpent
  * @param lesY[] tableau d'entiers : les coordonnées actuelle des lignes du serpent
@@ -192,7 +191,7 @@ void dessinerSerpent(int lesX[], int lesY[])
  */
 void progresser(int lesX[], int lesY[], char direction)
 {
-    for (int i = TAILLE_S - 1; i > 0; i--) 
+    for (int i = TAILLE_S - 1; i > 0; i--)
     {
         lesX[i] = lesX[i - 1]; // transforme les coordonées de chaque corp du serpent en les coordonées du corp suivant
         lesY[i] = lesY[i - 1];
@@ -215,48 +214,50 @@ void progresser(int lesX[], int lesY[], char direction)
     }
 }
 
-
 /**
  * @brief Cette fonction teste s’il y a eu frappe d’un caractère
  * au clavier et retourne 1 si c’est le cas et 0 sinon.
  */
- int kbhit(){
-	// la fonction retourne :
-	// 1 si un caractere est present
-	// 0 si pas de caractere present
+int kbhit()
+{
+    // la fonction retourne :
+    // 1 si un caractere est present
+    // 0 si pas de caractere present
 
-	int unCaractere=0;
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
+    int unCaractere = 0;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-	// mettre le terminal en mode non bloquant
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    // mettre le terminal en mode non bloquant
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-	ch = getchar();
+    ch = getchar();
 
-	// restaurer le mode du terminal
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
+    // restaurer le mode du terminal
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-	if(ch != EOF){
-		ungetc(ch, stdin);
-		unCaractere=1;
-	}
-	return unCaractere;
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        unCaractere = 1;
+    }
+    return unCaractere;
 }
 
-
-void disableEcho() {
+void disableEcho()
+{
     struct termios tty;
 
     // Obtenir les attributs du terminal
-    if (tcgetattr(STDIN_FILENO, &tty) == -1) {
+    if (tcgetattr(STDIN_FILENO, &tty) == -1)
+    {
         perror("tcgetattr");
         exit(EXIT_FAILURE);
     }
@@ -265,19 +266,20 @@ void disableEcho() {
     tty.c_lflag &= ~ECHO;
 
     // Appliquer les nouvelles configurations
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1)
+    {
         perror("tcsetattr");
         exit(EXIT_FAILURE);
     }
 }
 
-
-
-void enableEcho() {
+void enableEcho()
+{
     struct termios tty;
 
     // Obtenir les attributs du terminal
-    if (tcgetattr(STDIN_FILENO, &tty) == -1) {
+    if (tcgetattr(STDIN_FILENO, &tty) == -1)
+    {
         perror("tcgetattr");
         exit(EXIT_FAILURE);
     }
@@ -286,7 +288,8 @@ void enableEcho() {
     tty.c_lflag |= ECHO;
 
     // Appliquer les nouvelles configurations
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1)
+    {
         perror("tcsetattr");
         exit(EXIT_FAILURE);
     }
